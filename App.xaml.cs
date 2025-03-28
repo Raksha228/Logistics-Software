f6f9923204afe6ea6b962f33583bd3ddf6c34e19
SystemX <SystemX@DESKTOP-EM5BFAS>
2025-03-28 23:09:16 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Logistics_Software
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
